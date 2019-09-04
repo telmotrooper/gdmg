795a19f3d39f384e202d2086ae6f6ada0aa67dc3
Telmo "Trooper <telmo.trooper@gmail.com>
2019-09-04 11:45:01 -0300

    Added script to extract GNOME Shell Theme

---
+++ extractgst.sh
@@ -0,0 +1,17 @@
+# Script from:
+# https://wiki.archlinux.org/index.php/GDM#Log-in_screen_background_image 
+
+#!/bin/sh
+gst=/usr/share/gnome-shell/gnome-shell-theme.gresource
+workdir=${HOME}/shell-theme
+
+for r in `gresource list $gst`; do
+	r=${r#\/org\/gnome\/shell/}
+	if [ ! -d $workdir/${r%/*} ]; then
+	  mkdir -p $workdir/${r%/*}
+	fi
+done
+
+for r in `gresource list $gst`; do
+        gresource extract $gst $r >$workdir/${r#\/org\/gnome\/shell/}
+done
