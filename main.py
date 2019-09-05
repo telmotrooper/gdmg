9fbe3cf198d7e232d1f62d466925de612f00a22a
Telmo "Trooper <telmo.trooper@gmail.com>
2019-09-05 18:54:24 -0300

    More status output

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 
 import os, re, subprocess
-from print_logo import print_logo
+from func.print_logo import print_logo
+from func.colors import green, cyan, yellow
 
 print_logo()
 
-print("Extracting GNOME Shell Theme...")
+print(f"Extracting {cyan('GNOME Shell Theme')}...")
 os.system("./extractgst.sh")
 
 wallpaper_filename = subprocess.check_output(
   "ls -l | grep '.jpg\|.png' | awk '{print $9}'",
   shell=True, stderr=subprocess.STDOUT).decode().rstrip()
 
-print(f'"{wallpaper_filename}" found in current folder.')
+print(f"{green(wallpaper_filename)} found in current folder.")
 
-print(f'Copying "{wallpaper_filename}"" to theme folder...')
+print(f"Copying {green(wallpaper_filename)} to theme folder...")
 os.system(f"cp {wallpaper_filename} theme")
 
 
-print('Writing "gnome-shell-theme.gresource.xml" file...')
+print(f"Writing {green('gnome-shell-theme.gresource.xml')} file...")
 
 os.system(f"""echo '<?xml version="1.0" encoding="UTF-8"?>
 <gresources>
@@ -56,16 +57,16 @@ os.system(f"""echo '<?xml version="1.0" encoding="UTF-8"?>
 with open("theme/gnome-shell.css", "r") as file:
   filedata = file.read()
 
-print('Editing "gnome-shell.css" file...')
+print(f'Editing {green("gnome-shell.css")} file...')
 # Replace the definition of #lockDialogGroup with an empty string
 new_file = re.sub(r'(?s)(#lockDialogGroup {(?<={)(.*?)(?=})})', "", filedata)
 
 # TODO: Revemo direct reference to "wallpaper.jpg", file might have another name
-new_file += """#lockDialogGroup {
-  background: #2e3436 url(wallpaper.jpg) !important;
+new_file += """#lockDialogGroup \{
+  background: #2e3436 url(%s) !important;
   background-size: cover !important;
   background-repeat: no-repeat !important;
-}"""
+}""" % wallpaper_filename
 
 with open("theme/gnome-shell.css", "w") as file:
   file.write(new_file)
@@ -80,10 +81,13 @@ xrandr_output = subprocess.check_output(
 
 # for line in xrandr_output:
   
-
+print("Processing wallpaper for setup with two displays...")
 os.system(f"convert -background none \
 \( {wallpaper_filename} -resize 1920x1080! \) \
 \( {wallpaper_filename} -resize 1440x900! \) +append theme/wallpaper.jpg")
 
 os.system("cd theme && glib-compile-resources gnome-shell-theme.gresource.xml")
+
+print("Super user privileges are required to install the modified theme.\n")
 os.system("sudo mv theme/gnome-shell-theme.gresource /usr/share/gnome-shell/")
+print(f"\nModified theme installed. {yellow(':)')}\n")
